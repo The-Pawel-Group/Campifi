4c52b4931df854459822f72151b4ed45005c92d2
rayk216 <raykoren@gmail.com>
2016-06-28 10:25:25 -0600

    added edit site functionality

--- campifi/routes/campsite.js
+++ campifi/routes/campsite.js
@@ -3,27 +3,50 @@ var router = express.Router();
 var knex = require('../db/knex');
 var db = require('../db/api');
 var auth = require('../auth');
-router.get('/', function(req, res, next) {
-  res.render('campsite');
-});
-
+// router.get('/', function(req, res, next) {
+//   res.render('campsite');
+// });
+// render sites
 router.get('/:id', function(req, res, next) {
-  return Promise.all([
-  knex('site').select().where('site.id', req.params.id).first()
-])
-  .then(function(data) {
-    console.log(data);
-    res.render('campsite', {site: data});
-  });
+    return Promise.all([
+        knex('site').select().where('site.id', req.params.id).first()
+    ]).then(function(data) {
+        console.log(data);
+        res.render('campsite', {
+            site: data
+        });
+    });
+});
+// Delete site //
+router.get('/delete/:id', function(req, res, next) {
+    knex('site').where({
+        id: req.params.id
+    }).del().then(function(data) {
+        res.redirect('/home');
+    }).catch(function(error) {
+        next(error);
+    });
+});
+// Edit site //
+router.get('/edit/:id', function(req, res, next) {
+    knex('site').select().where('site.id', req.params.id).first().then(function(data) {
+        res.render('site_edit', {
+            site: data
+        });
+    }).catch(function(error) {
+        next(error);
+    });
 });
 
-router.get('/delete/:id', function(req, res, next) {
-  console.log(req.params.id)
- knex('site').where({id: req.params.id}).del().then(function(data){
-   res.redirect('/home');
- }).catch(function(error) {
-     next(error);
- });
+router.post('/edit/:id', function(req, res, next) {
+    knex('site').where({
+        id: req.params.id
+    }).update(req.body).then(function() {
+        res.redirect('/campsite/' + req.params.id);
+
+    }).catch(function(error) {
+        next(error);
+    });
 });
 
 module.exports = router;
