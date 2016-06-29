466c186f87ff000b71b5787b840f0f2fa6d3f8d3
Pawel <Pawel.Maciulewski@gmail.com>
2016-06-29 15:37:39 -0600

    define roles

--- campifi/routes/campsite.js
+++ campifi/routes/campsite.js
@@ -7,21 +7,32 @@ var auth = require('../auth');
 // Render sites //
 router.get('/:id', auth.isNotLoggedIn, function(req, res, next) {
     return Promise.all([
-        knex('camper').select('site.id', 'camper.username', 'site.name', 'site.image', 'site.longitude', 'site.latitude', 'site.description')
+        knex('camper').select('site.id', 'camper.username', 'site.camper_id', 'site.name', 'site.image', 'site.longitude', 'site.latitude', 'site.description')
         .join('site', 'camper.id', 'site.camper_id').where('site.id', req.params.id).first(),
         knex('comment').select('comment.message', 'camper.username')
         .join('camper', 'camper.id', 'comment.camper_id')
         .where('site_id', req.params.id)
     ]).then(function(data) {
+      console.log(data[1])
+      if (req.session.camperId === data[0].camper_id  || req.session.camperId === 4) {
         res.render('campsite', {
             site: data[0],
             comment: data[1],
-            id: req.session.camperId
+            id: req.session.camperId,
+            permission: true
         });
+      } else {
+        res.render('campsite', {
+            site: data[0],
+            comment: data[1],
+            id: req.session.camperId,
+            permission: false
+        });
+      }
     });
 });
 // Delete site //
-router.get('/delete/:id', function(req, res, next) {
+router.get('/delete/:id', auth.isNotLoggedIn, function(req, res, next) {
     knex('site').where({
         id: req.params.id
     }).del().then(function(data) {
@@ -53,14 +64,13 @@ router.post('/edit/:id', function(req, res, next) {
 });
 // Add Comment //
 router.post('/comment/:id', function(req, res, next) {
-  console.log(req.params.id);
-  console.log(req.body);
-  knex('comment').insert(req.body).then(function() {
-    res.redirect('/campsite/' + req.params.id);
-  }).catch(function(error) {
-    next(error);
-  });
+knex('comment').insert(req.body).then(function() {
+res.redirect('/campsite/' + req.params.id);
+}).catch(function(error) {
+next(error);
 });
+});
+
 
 
 module.exports = router;
