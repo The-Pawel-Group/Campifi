0fba7293e93b5ff7eab18d2038e8d08cde0510ab
rayk216 <raykoren@gmail.com>
2016-06-29 10:23:05 -0600

    added near by page and basic map style and nearby route.js

---
+++ campifi/routes/nearby.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var knex = require('../db/knex');
+var db = require('../db/api');
+var auth = require('../auth');
+
+router.get('/', function(req, res , next) {
+  res.render('nearby');
+});
+
+module.exports = router;
