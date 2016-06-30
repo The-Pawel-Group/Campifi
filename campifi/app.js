bcd4ada4f7f65ef75c1b4498fb5b4ff30c022ea0
Pawel <Pawel.Maciulewski@gmail.com>
2016-06-29 19:31:37 -0600

    add app file changes

--- campifi/app.js
+++ campifi/app.js
@@ -38,7 +38,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({keys: [process.env.SESSION_KEY1, process.env.SESSION_KEY2]}))
+app.use(session({keys: [process.env.SESSION_KEY1, process.env.SESSION_KEY2]}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
